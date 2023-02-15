c9989073ba8d68988a67afa7ac836b79b6b98438
Adriel <adriel.fsantos@outlook.com>
2023-02-14 22:32:25 -0300

    Update with express

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
-const fs = require('fs/promises');
-const http = require('http');
+const path = require('path');
+const express = require('express');
 
+/* CONSTS */
 const HOST = 'localhost';
 const PORT = 3000;
+const PAGES_DIR = path.join(__dirname, 'pages');
 
-function sendPage(response, content) {
-  response.setHeader('Content-Type', 'text/html');
-  response.writeHead(200);
-  response.end(content);
-}
+/* APP */
+const app = express();
 
-const app = http.createServer(async (req, res) => {
-  const url = req.url;
-  switch (url) {
-    case '/':
-      fs.readFile(__dirname + '/pages/index.html').then((content) => {
-        sendPage(res, content);
-      });
-      break;
-    case '/contact':
-      fs.readFile(__dirname + '/pages/contact.html').then((content) => {
-        sendPage(res, content);
-      });
-      break;
-    case '/about':
-      fs.readFile(__dirname + '/pages/about.html').then((content) => {
-        sendPage(res, content);
-      });
-      break;
-    default:
-      fs.readFile(__dirname + '/pages/404.html').then((content) => {
-        res.setHeader('Content-Type', 'text/html');
-        res.writeHead(404);
-        res.end(content);
-      });
-  }
+/* APP - ROUTES */
+app.get('/', (req, res) => {
+  res.sendFile(`${PAGES_DIR}/index.html`);
 });
 
+app.get('/contact', (req, res) => {
+  res.sendFile(`${PAGES_DIR}/contact.html`);
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(`${PAGES_DIR}/about.html`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(`${PAGES_DIR}/404.html`);
+});
+
+/* APP - SERVER */
 app.listen(PORT, HOST, () => {
   console.log(`SERVER RUNNING => http://${HOST}:${PORT}`);
 });
